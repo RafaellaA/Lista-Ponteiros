f2ece32ed16248e32f70656c26bca497f66773ab
RafaellaA <42594297+RafaellaA@users.noreply.github.com>
2018-09-10 16:41:29 -0300

    Create questão1.c

---
+++ questão1.c
@@ -0,0 +1,3 @@
+A utilidade da aprendizagem do uso de ponteiros para aplicações em engenharia se deve pela importância que essa função exerce através 
+de endereços de memória por meio da passagem de parâmetros por referência, passando o endereço ao invés da cópia,sendo muito utilizado no
+retorno e na passagem de funções. Ou seja, há um menor gasto de memória durante a execução do programa.
