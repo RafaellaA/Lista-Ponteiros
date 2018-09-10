9dd610a34f9cb1b08f164f5e431a0f2039a27e8d
RafaellaA <42594297+RafaellaA@users.noreply.github.com>
2018-09-10 19:31:26 -0300

    Create Questão8.c

---
+++ Questão8.c
@@ -0,0 +1,5 @@
+Questão 08)
+
+p = mat + 1;
+x = (*mat)++;
+Essas expressões são válidas . Na primeira p recebe o  endereço  de memória  da posição v(0)  e avança  4 bytes ( valor referente  a um inteiro) indo para  a segunda posição do vetor  , já x recebe o valor da posição inicial e o incrementa . As outras expressões são inválidas , pois elas tentam modificar o endereço do vetor .  
